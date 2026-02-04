4b7ff2975727077ae40fea86df70541239e6f85f
MarkussK07 <karklismarkuss@gmail.com>
2026-02-04 10:44:16 +0200

    labojums
    yey

--- app.py
+++ app.py
@@ -1,55 +1,87 @@
 from flask import Flask, render_template, request, redirect, session
-import sqlite3
+import sqlite3 
+import hashlib
 
 app = Flask(__name__)
-app.secret_key = "supersecret"
+app.secret_key = "secret123"
 
-DB = "olimpiades.db"
+USERNAME = "arturix@arturix.lv"
+HASH ="9e03f8af886ffe78a5fcd5e4827af673" #Parole: Tests123!
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = ""
+
+    if request.method == "POST":
+        username= request.form.get("username") 
+        password = request.form.get("pwd")
+        print(username)
+        print(password)
+
+        hashed = hashlib.md5(password.encode("utf-8")).hexdigest()
+        print (hashed)
+
+        if username == USERNAME and hashed == HASH:
+            return render_template("home.html", username-username)
+        else:
+            error = "Nepareizs lietotājvārds vai parole"
+
+        return render_template("login.html", error-error)
+
+
+DB = "PD_DB.db"
 
 def get_db():
     return sqlite3.connect(DB)
 
+
 @app.route("/", methods=["GET", "POST"])
-def login():
+def index():
     if request.method == "POST":
-        email = request.form["email"]
-        parole = request.form["parole"]
+        mode = request.form.get("mode")
+
+        email = request.form.get("email")
+        parole = request.form.get("parole")
 
         db = get_db()
         cur = db.cursor()
-        cur.execute(
-            "SELECT id, vards FROM lietotajs WHERE epasts=? AND parole=?",
-            (email, parole)
-        )
-        user = cur.fetchone()
 
-        if user:
+        if mode == "login":
+            cur.execute(
+                "SELECT id, vards FROM lietotajs WHERE epasts=? AND parole=?",
+                (email, parole)
+            )
+            user = cur.fetchone()
+
+            if user:
+                session["user_id"] = user[0]
+                session["vards"] = user[1]
+                return redirect("/home")
+
+        elif mode == "register":
+            vards = request.form.get("vards")
+            uzvards = request.form.get("uzvards")
+
+            cur.execute("""
+                INSERT INTO lietotajs (vards, uzvards, epasts, parole, loma)
+                VALUES (?, ?, ?, ?, 'skolens')
+            """, (vards, uzvards, email, parole))
+
+            db.commit()
+
+            cur.execute(
+                "SELECT id, vards FROM lietotajs WHERE epasts=?",
+                (email,)
+            )
+            user = cur.fetchone()
             session["user_id"] = user[0]
             session["vards"] = user[1]
             return redirect("/home")
 
+        db.close()
+
     return render_template("login.html")
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        vards = request.form["vards"]
-        uzvards = request.form["uzvards"]
-        email = request.form["email"]
-        parole = request.form["parole"]
-        klase = request.form["klase"]
-
-        db = get_db()
-        cur = db.cursor()
-        cur.execute("""
-            INSERT INTO lietotajs (vards, uzvards, epasts, parole, loma)
-            VALUES (?, ?, ?, ?, 'skolens')
-        """, (vards, uzvards, email, parole))
-        db.commit()
-
-        return redirect("/")
-
-    return render_template("register.html")
 
 @app.route("/home")
 def home():
@@ -57,10 +89,12 @@ def home():
         return redirect("/")
     return render_template("home.html", vards=session["vards"])
 
+
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
